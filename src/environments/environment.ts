4505e62cd65b08d36fb410672b75bbb11fee8311
Santiago Cruz <mosu@gft.com>
2020-02-25 12:28:36 -0600

    Add credential, remove later

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -5,17 +6,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxx-xxxxxx",
-    authDomain: "xxxx-xxxxxx",
-    databaseURL: "xxxx-xxxxxx",
-    projectId: "xxxx-xxxxxx",
-    storageBucket: "xxxx-xxxxxx",
-    messagingSenderId: "xxxx-xxxxxx",
-    appId: "xxxx-xxxxxx",
-    measurementId: "xxxx-xxxxxx",
+    apiKey: "AIzaSyAC2bLDP3IBEDtfJrv9Z4PrkKOGJNxeiLE",
+    authDomain: "test-web-app-angular.firebaseapp.com",
+    databaseURL: "https://test-web-app-angular.firebaseio.com",
+    projectId: "test-web-app-angular",
+    storageBucket: "test-web-app-angular.appspot.com",
+    messagingSenderId: "768352133366",
+    appId: "1:768352133366:web:bb25b12da3d45ee4284cd7",
+    measurementId: "G-2RVM2SEN3H"
   },
   urlNews: 'http://newsapi.org/v2/top-headlines',
-  apiKeyNews: 'xxxx'
+  apiKeyNews: 'c9b3bf15fa934f548352fb1182e983fb'
 };
 
 /*
